17a5670469b1538b7617e3e872bfca55c1f6305e
schlaffeck <slawomir.moriak@gmail.com>
2017-01-26 13:52:40 +0100

    Settings - templated activity and move from action bar menu

--- app/src/main/java/com/slamcode/goalcalendar/MonthlyGoalsActivity.java
+++ app/src/main/java/com/slamcode/goalcalendar/MonthlyGoalsActivity.java
@@ -1,5 +1,6 @@
 package com.slamcode.goalcalendar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -148,13 +149,11 @@ public class MonthlyGoalsActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == com.slamcode.goalcalendar.R.id.action_settings) {
+            Intent goToSettings = new Intent(this, SettingsActivity.class);
+            this.startActivity(goToSettings);
             return true;
         }
 
