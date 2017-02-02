b6cebd2920a6ba3db9527bb9b50899c72f462998
schlaffeck <slawomir.moriak@gmail.com>
2017-02-02 15:33:07 +0100

    Monthly goals - view model - Unit tests #2

--- app/src/test/java/com/slamcode/goalcalendar/view/viewmodel/MonthlyGoalsViewModelTest.java
+++ app/src/test/java/com/slamcode/goalcalendar/view/viewmodel/MonthlyGoalsViewModelTest.java
@@ -5,27 +5,30 @@ import android.test.mock.MockContext;
 import android.view.LayoutInflater;
 
 import com.slamcode.collections.CollectionUtils;
-import com.slamcode.goalcalendar.data.CategoryRepository;
 import com.slamcode.goalcalendar.data.MonthlyPlansRepository;
 import com.slamcode.goalcalendar.data.PersistenceContext;
 import com.slamcode.goalcalendar.data.UnitOfWork;
 import com.slamcode.goalcalendar.data.model.CategoryModel;
-import com.slamcode.goalcalendar.data.model.DailyPlanModel;
 import com.slamcode.goalcalendar.data.model.MonthlyPlansModel;
 import com.slamcode.goalcalendar.planning.DateTimeHelper;
 import com.slamcode.goalcalendar.planning.FrequencyPeriod;
 import com.slamcode.goalcalendar.planning.Month;
 import com.slamcode.goalcalendar.view.CategoryListViewAdapter;
 import com.slamcode.goalcalendar.view.lists.ListAdapterProvider;
-import com.slamcode.goalcalendar.view.lists.ListViewDataAdapter;
-
-import static org.mockito.Mockito.*;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatcher;
 import org.mockito.Matchers;
 import org.mockito.Mockito;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by moriasla on 02.02.2017.
@@ -186,5 +189,85 @@ public class MonthlyGoalsViewModelTest {
         verify(repository).findForMonth(Matchers.eq(2017), Matchers.eq(Month.APRIL));
         verify(repository).add(Matchers.notNull(MonthlyPlansModel.class));
         verify(adapter).updateMonthlyPlans(Matchers.notNull(MonthlyPlansModel.class));
+    }
+
+
+    @Test
+    public void monthlyGoalsViewModel_copyFromPreviousMonth_noData_test()
+    {
+        // mocks
+        Context context = new MockContext();
+        LayoutInflater inflater = Mockito.mock(LayoutInflater.class);
+
+        final CategoryListViewAdapter adapter = Mockito.mock(CategoryListViewAdapter.class);
+        ListAdapterProvider adapterProvider = Mockito.mock(ListAdapterProvider.class);
+        when(adapterProvider.provideCategoryListViewAdapter(context, inflater)).thenReturn(adapter);
+
+        PersistenceContext persistenceContext = Mockito.mock(PersistenceContext.class);;
+
+        UnitOfWork unitOfWork = Mockito.mock(UnitOfWork.class);
+
+        MonthlyPlansRepository repository = Mockito.mock(MonthlyPlansRepository.class);
+
+        final Month nextMonthEnum = Month.getNextMonth();
+        final Month currentMonthEnum = Month.getCurrentMonth();
+        final int year = DateTimeHelper.getCurrentYear();
+
+        MonthlyPlansModel currentMonth =
+                new MonthlyPlansModel(1,
+                        year,
+                        currentMonthEnum);
+        currentMonth.setCategories(
+                CollectionUtils.createList(new CategoryModel(1, "Kat 1", FrequencyPeriod.Month, 2)));
+
+        MonthlyPlansModel nextMonth =
+                new MonthlyPlansModel(1,
+                        year,
+                        nextMonthEnum);
+
+        // arrange
+        when(persistenceContext.createUnitOfWork()).thenReturn(unitOfWork);
+        when(unitOfWork.getMonthlyPlansRepository()).thenReturn(repository);
+        when(repository.findForMonth(DateTimeHelper.getCurrentYear(), Month.getCurrentMonth()))
+                .thenReturn(currentMonth);
+        when(repository.findForMonth(DateTimeHelper.getCurrentYear(), Month.getNextMonth()))
+                .thenReturn(nextMonth);
+        when(repository.findLast(Matchers.any(com.android.internal.util.Predicate.class))).thenReturn(currentMonth);
+
+        // act
+        MonthlyGoalsViewModel viewModel = new MonthlyGoalsViewModel(
+                context,
+                inflater,
+                persistenceContext,
+                adapterProvider);
+        viewModel.setYearAndMonth(DateTimeHelper.getCurrentYear(), Month.getNextMonth());
+        viewModel.copyCategoriesFromPreviousMonth();
+
+        // assert
+        verify(persistenceContext, times(2)).createUnitOfWork();
+        verify(unitOfWork, times(2)).getMonthlyPlansRepository();
+        verify(unitOfWork, times(2)).complete();
+        verify(repository).findForMonth(Matchers.eq(year), Matchers.eq(nextMonthEnum));
+        verify(adapter).updateMonthlyPlans(Matchers.eq(nextMonth));
+
+        verify(repository, times(1)).findLast(Matchers.isNotNull(com.android.internal.util.Predicate.class));
+
+        ArgumentMatcher<CategoryModel> matcher = new ArgumentMatcher<CategoryModel>(){
+
+            @Override
+            public boolean matches(Object argument) {
+                CategoryModel category = (CategoryModel)argument;
+
+                assertEquals("Kat 1", category.getName());
+                assertNotNull(category.getId());
+                assertEquals(2, category.getFrequencyValue());
+                assertEquals(FrequencyPeriod.Month, category.getPeriod());
+
+                assertNotNull(category.getDailyPlans());
+                assertEquals(nextMonthEnum.getDaysCount(year), category.getDailyPlans().size());
+                return true;
+            }
+        };
+        verify(adapter, times(1)).addOrUpdateItem(Matchers.argThat(matcher));
     }
 }
