98134ab6daeb08e5380e1b873c70564df0491baa
schlaffeck <genki6DAMA!>
2017-05-03 19:39:43 +0200

    gradle - multidex configured
    # Conflicts:
    #	app/app.iml
    #	app/build.gradle
    #	app/src/main/java/com/slamcode/goalcalendar/dagger2/ComposableApplication.java

--- app/src/main/java/com/slamcode/goalcalendar/dagger2/ComposableApplication.java
+++ app/src/main/java/com/slamcode/goalcalendar/dagger2/ComposableApplication.java
@@ -1,13 +1,8 @@
 package com.slamcode.goalcalendar.dagger2;
 
-import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
-
-import com.slamcode.goalcalendar.data.dagger2.*;
-import com.slamcode.goalcalendar.service.dagger2.ServiceDagger2Module;
-import com.slamcode.goalcalendar.view.dagger2.ViewDagger2Module;
 
 /**
  * Created by moriasla on 04.01.2017.
@@ -24,14 +19,15 @@ public final class ComposableApplication extends MultiDexApplication {
         this.applicationComponent = Dagger2ComponentContainer.getApplicationDagger2Component(this);
     }
 
+    public ApplicationDagger2Component getApplicationComponent()
+    {
+        return this.applicationComponent;
+    }
+
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
     }
 
-    public ApplicationDagger2Component getApplicationComponent()
-    {
-        return this.applicationComponent;
-    }
 }
