b0504ffb2d0afcfe6ca5fe9922c5cfba9a730008
schlaffeck <slawomir.moriak@gmail.com>
2017-05-25 13:45:26 +0200

    data - edit description dialog removing butterknife dependency

--- app/src/main/java/com/slamcode/goalcalendar/view/dialogs/EditDailyPlansViewModelDialog.java
+++ app/src/main/java/com/slamcode/goalcalendar/view/dialogs/EditDailyPlansViewModelDialog.java
@@ -7,13 +7,12 @@ import android.widget.TextView;
 
 import com.slamcode.goalcalendar.R;
 import com.slamcode.goalcalendar.view.dialogs.base.AddEditDialog;
+import com.slamcode.goalcalendar.view.utils.ViewBinder;
+import com.slamcode.goalcalendar.view.utils.ViewOnClick;
+import com.slamcode.goalcalendar.view.utils.ViewReference;
 import com.slamcode.goalcalendar.viewmodels.DailyPlansViewModel;
 
 import java.util.Date;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * Created by smoriak on 25/05/2017.
@@ -21,10 +20,10 @@ import butterknife.OnClick;
 
 public class EditDailyPlansViewModelDialog extends AddEditDialog<EditDailyPlansViewModelDialog.DailyPlansDialogData> {
 
-    @BindView(R.id.edit_dailyPlans_dialog_description_edittext)
+    @ViewReference(R.id.edit_dailyPlans_dialog_description_edittext)
     EditText descriptionEditText;
 
-    @BindView(R.id.edit_dailyPlans_dialog_header_textview)
+    @ViewReference(R.id.edit_dailyPlans_dialog_header_textview)
     TextView headerTextView;
 
     @Override
@@ -32,7 +31,7 @@ public class EditDailyPlansViewModelDialog extends AddEditDialog<EditDailyPlansV
 
         View view = inflater.inflate(R.layout.monthly_goals_edit_daily_plans_dialog_layout, null);
 
-        ButterKnife.bind(this, view);
+        ViewBinder.bindViews(this, view);
 
         this.headerTextView.setText(
                 String.format(
@@ -47,13 +46,16 @@ public class EditDailyPlansViewModelDialog extends AddEditDialog<EditDailyPlansV
     }
 
     @Override
-    @OnClick(R.id.edit_dailyPlans_dialog_confirm_button)
+    @ViewOnClick(R.id.edit_dailyPlans_dialog_confirm_button)
     protected void commitChanges() {
         this.getModel().dailyPlansViewModel.setDescription(this.descriptionEditText.getText().toString());
+        this.setConfirmed(true);
+        this.getDialog().dismiss();
+        this.onDialogClosed();
     }
 
     @Override
-    @OnClick(R.id.edit_dailyPlans_dialog_cancel_button)
+    @ViewOnClick(R.id.edit_dailyPlans_dialog_cancel_button)
     protected void cancelChanges()
     {
         super.cancelChanges();
