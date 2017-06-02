81bf06ce51c820432c4af90fd26ee6fb27704547
schlaffeck <slawomir.moriak@gmail.com>
2017-06-02 12:54:53 +0200

    summary - differentiate colors on adjacent slices

--- app/src/main/java/com/slamcode/goalcalendar/view/charts/data/hellocharts/HelloChartsViewDataBinder.java
+++ app/src/main/java/com/slamcode/goalcalendar/view/charts/data/hellocharts/HelloChartsViewDataBinder.java
@@ -75,12 +75,25 @@ public class HelloChartsViewDataBinder implements ChartViewDataBinder<PieChartVi
             }
 
             PieChartData data = new PieChartData(slices);
+            this.assureDifferentSlicesColors(slices);
             data.setHasLabelsOnlyForSelected(true);
             data.setSlicesSpacing(0);
             pieChartView.setChartRotationEnabled(false);
             pieChartView.setSelectedValueToggleEnabled(true);
             pieChartView.setPieChartData(data);
         }
+    }
+
+    private void assureDifferentSlicesColors(List<SliceValue> slices) {
+        if(slices == null || slices.size() < 2)
+            return;
+
+        final int noOfColors = 5;
+        if(slices.size() % noOfColors != 1)
+            return;
+
+        SliceValue lastOne = slices.get(slices.size()-1);
+        lastOne.setColor(this.getNextColor(lastOne.getColor()));
     }
 
     private int countAllExpectedTasks(Iterable<CategoryPlansViewModel> itemsSource) {
@@ -119,24 +132,29 @@ public class HelloChartsViewDataBinder implements ChartViewDataBinder<PieChartVi
                         category.getNoOfExpectedTasks()));
     }
 
-    private int getNextColor()
+    private int getNextColor(int previousColor)
     {
-        int color = this.lastColor;
-        if(this.lastColor == ChartUtils.COLOR_BLUE)
-                color = ChartUtils.COLOR_GREEN;
+        int color = 0;
+        if(previousColor == ChartUtils.COLOR_BLUE)
+            color = ChartUtils.COLOR_GREEN;
 
-        else if(this.lastColor == ChartUtils.COLOR_GREEN)
+        else if(previousColor == ChartUtils.COLOR_GREEN)
             color = ChartUtils.COLOR_ORANGE;
 
-        else if(this.lastColor == ChartUtils.COLOR_ORANGE)
+        else if(previousColor == ChartUtils.COLOR_ORANGE)
             color = ChartUtils.COLOR_RED;
 
-        else if(this.lastColor == ChartUtils.COLOR_RED)
+        else if(previousColor == ChartUtils.COLOR_RED)
             color = ChartUtils.COLOR_VIOLET;
 
         else color = ChartUtils.COLOR_BLUE;
 
-        return this.lastColor = color;
+        return color;
+    }
+
+    private int getNextColor()
+    {
+        return this.lastColor = this.getNextColor(this.lastColor);
     }
 
     class CategoryPropertyChangedCallback extends Observable.OnPropertyChangedCallback{
