9579734e5d0af0eb8d1cc77d61021e706729c2fd
schlaffeck <thornis@o2.pl>
2018-12-23 08:50:46 +0100

    backup view - list item view and view model

---
+++ app/src/main/java/com/slamcode/goalcalendar/viewmodels/BackupSourceViewModel.java
@@ -0,0 +1,31 @@
+package com.slamcode.goalcalendar.viewmodels;
+
+import com.slamcode.goalcalendar.backup.BackupSourceDataProvider;
+import com.slamcode.goalcalendar.planning.DateTime;
+
+import java.util.Locale;
+
+public class BackupSourceViewModel {
+
+    private final BackupSourceDataProvider dataProvider;
+    private final DateTime lastBackupDateTime;
+
+    BackupSourceViewModel(BackupSourceDataProvider dataProvider, DateTime lastBackupDateTime)
+    {
+        this.dataProvider = dataProvider;
+        this.lastBackupDateTime = lastBackupDateTime;
+    }
+
+    public String getName() {
+        return this.dataProvider.getDisplayData(Locale.getDefault()).getSourceName();
+    }
+
+    public boolean isHasBackup() {
+        return this.lastBackupDateTime != null;
+    }
+
+    public DateTime getLastBackupDateTime()
+    {
+        return this.lastBackupDateTime;
+    }
+}
